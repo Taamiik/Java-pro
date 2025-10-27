b7973ca3bee58a1c704b9217e8373297c4fceae8
Томаев Тамик Казбекович <ttomaev@nota.tech>
2025-10-27 12:25:12 +0300

    task3

--- src/main/java/org/practice/Main.java
+++ src/main/java/org/practice/Main.java
@@ -1,6 +1,6 @@
 package org.practice;
 
-import org.practice.task2.Streams;
+import org.practice.task3.ThreadPool;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +8,39 @@ public class Main {
         // TestRunner.runTests(TestClass.class);
 
         // task2
-        Streams.start();
+        // Streams.start();
+
+        // task3
+        ThreadPool threadPool = new ThreadPool(2);
+
+        for (int i = 1; i < 10; i++) {
+            int taskId = i;
+            threadPool.execute(() -> {
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("Начало задачи " + taskId + " в потоке " + Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        threadPool.shutdown();
+        threadPool.awaitTermination();
+        System.out.println("Главный поток  = Финал - " + Thread.currentThread().getName());
+
+        try {
+            threadPool.execute(() -> {
+                System.out.println("Эта задача не должна быть выполнена: " + Thread.currentThread().getName());
+            });
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 }
